499ed317451ab450a5924c1689cee85e2de4aa16
LurkAndLoiter <93033540+LurkAndLoiter@users.noreply.github.com>
2025-11-06 16:12:56 -0500

    feat: initial firefox support;pre readme;unsigned extension issue with firefox

--- install.sh
+++ install.sh
@@ -10,27 +10,37 @@ SH_PATH="$SCRIPT_DIR/mpris_helper.sh"
 JSON_PATH="$SCRIPT_DIR/mpris_helper.json"
 echo -e "${BLUE}Detecting installed browsers...${NC}"
 browsers=(
-    "chromium google-chrome-stable google-chrome"
-    "chromium chromium chromium"
-    "brave brave-browser BraveSoftware/Brave-Browser"
-    "brave brave-browser-beta BraveSoftware/Brave-Browser-Beta"
-    "brave brave-browser-nightly BraveSoftware/Brave-Browser-Nightly"
-    "vivaldi vivaldi-stable vivaldi"
-    "vivaldi vivaldi-snapshot vivaldi-snapshot"
-    "edge microsoft-edge-stable microsoft-edge"
-    "edge microsoft-edge-beta microsoft-edge-beta"
-    "edge microsoft-edge-dev microsoft-edge-dev"
-    "opera opera opera"
-    "opera opera-beta opera-beta"
-    "opera opera-developer opera-developer"
+    "chromium google-chrome-stable google-chrome chromium"
+    "chromium chromium chromium chromium"
+    "brave brave-browser BraveSoftware/Brave-Browser chromium"
+    "brave brave-browser-beta BraveSoftware/Brave-Browser-Beta chromium"
+    "brave brave-browser-nightly BraveSoftware/Brave-Browser-Nightly chromium"
+    "vivaldi vivaldi-stable vivaldi chromium"
+    "vivaldi vivaldi-snapshot vivaldi-snapshot chromium"
+    "edge microsoft-edge-stable microsoft-edge chromium"
+    "edge microsoft-edge-beta microsoft-edge-beta chromium"
+    "edge microsoft-edge-dev microsoft-edge-dev chromium"
+    "opera opera opera chromium"
+    "opera opera-beta opera-beta chromium"
+    "opera opera-developer opera-developer chromium"
+    "firefox firefox .mozilla firefox"
+    "firefox librewolf .librewolf firefox"
+    "firefox zen-browser .zen firefox"
 )
 
 echo -e "${BLUE}\nBuilding Native Messaging Hosts directories...${NC}"
 for b in "${browsers[@]}"; do
-    read -r name cmd config <<< "$b"
+    read -r name cmd config browsertype <<< "$b"
     if command -v "$cmd" &> /dev/null; then
         echo -e "\n${GREEN}Found: $cmd${NC}"
-        host_dir="$HOME/.config/$config/NativeMessagingHosts"
+        if [ "$browsertype" == "chromium" ]; then
+          host_dir="$HOME/.config/$config/NativeMessagingHosts"
+        elif [ "$browsertype" == "firefox" ]; then
+          host_dir="$HOME/$config/native-messaging-hosts"
+          JSON_PATH="$SCRIPT_DIR/mpris_helper.ff.json"
+        else
+          continue
+        fi
         update_path="$host_dir/mpris_helper.sh"
         sed_safe_host_dir=$(echo "$update_path" | sed 's/\//\\\//g')
         mkdir -p "$host_dir"
@@ -46,5 +56,6 @@ for b in "${browsers[@]}"; do
 done
 
 # reset to blank
-sed -i "s|\"path\": \".*\"|\"path\": \"blank\"|" "$JSON_PATH"
+sed -i "s|\"path\": \".*\"|\"path\": \"blank\"|" "$SCRIPT_DIR/mpris_helper.json"
+sed -i "s|\"path\": \".*\"|\"path\": \"blank\"|" "$SCRIPT_DIR/mpris_helper.ff.json"
 sed -i "s|^browser=.*|browser=blank|" "$SH_PATH"
